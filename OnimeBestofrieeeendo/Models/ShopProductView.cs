b75390d835b0259aa8a3e7d481899d3b0076025f
DeusFlow <deuswork@icloud.com>
2025-05-27 10:13:39 +0200

    worked with products section

---
+++ OnimeBestofrieeeendo/Models/ShopProductView.cs
@@ -0,0 +1,11 @@
+namespace Onime.Models;
+
+public class ShopProductView
+{
+    public int ShopItemId { get; set; }       // id из таблицы shop_items
+    public int ItemId { get; set; }           // id товара из таблицы shop_items
+    public string Name { get; set; } = "";    // название из products
+    public string ImageUrl { get; set; } = ""; // картинка из products
+    public decimal Price { get; set; }        // цена из shop_items
+    public int Quantity { get; set; }         // количество из shop_items
+}
