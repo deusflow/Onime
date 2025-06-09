6de617f13d0a4dc263c0da4fa1f867ca59880081
DeusFlow <deuswork@icloud.com>
2025-06-09 19:43:19 +0200

    rework of shop section

--- OnimeBestofrieeeendo/Models/ShopProductView.cs
+++ OnimeBestofrieeeendo/Models/ShopProductView.cs
@@ -2,12 +2,13 @@ namespace OnimeBestofrieeeendo.Models;
 
 public class ShopProductView
 {
-    public int ShopItemId { get; set; }       // id из таблицы shop_items
-    public int ItemId { get; set; }           // id товара из таблицы shop_items
-    public string Name { get; set; } = "";    // название из products
+    public int ShopItemId { get; set; } // id из таблицы shop_items
+    public int ItemId { get; set; } // id товара из таблицы shop_items
+    public string Name { get; set; } = ""; // название из products
     public string ImageUrl { get; set; } = ""; // картинка из products
-    public decimal Price { get; set; }        // цена из shop_items
-    public int Quantity { get; set; }         // количество из shop_items
-    
+    public decimal Price { get; set; } // цена из shop_items
+    public int Quantity { get; set; } // количество из shop_items
+
     public string Description { get; set; } = ""; // описание товара
+    public string Rarity { get; set; } = "Common"; // редкость товара
 }
